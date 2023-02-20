0746245a763272854ad4c8290e5c5f32cf28e760
Yekta <yekta.360@hotmail.fr>
2023-02-20 16:54:50 +0100

    feat(treeview): add component (#3)
    * feat(eslintrc): :sparkles: add eslint config
    
    * feat(TreeView): :sparkles: add component
    
    * feat(App): :sparkles: add TreeView
    
    * feat(package): :package: add material design icons
    
    * feat(main): :sparkles: add material design icons
    
    * feat(assets): :bento: :lipstick: add logo
    
    * fixup! feat(TreeView): :sparkles: add component
    
    * fixup! feat(App): :sparkles: add TreeView
    
    * feat(README): :memo: update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import TreeView from "./components/TreeView.vue";
+
+const tree = reactive({
+  firstname: "john",
+  lastname: "doe",
+  location: {
+    address: "lorem ipsum dolor sit amet",
+    zipcode: "XXXX",
+  },
+  cart: ["apple", "banana", "coconut"],
+  null: null,
+});
+</script>
+
 <template>
   <main>
-    HelloWorld
+    <v-container>
+      <v-img class="mb-5" width="300" src="eulerian.png" alt="eulerian logo" />
+      <v-row>
+        <v-col cols="6">
+          <v-sheet border rounded class="pa-5" color="grey-lighten-3">
+            {{ tree }}
+          </v-sheet>
+        </v-col>
+        <v-col cols="6">
+          <v-card>
+            <v-list>
+              <TreeView :tree="tree" />
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </main>
 </template>
